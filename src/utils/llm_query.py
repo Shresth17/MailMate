69efdd5cb29836243a07aee303942c84be61eb1d
Shresth Sharma <shresthsharma1711@gmail.com>
2026-03-12 17:34:17 +0530

    Add embedding query helper

--- src/utils/llm_query.py
+++ src/utils/llm_query.py
@@ -6,3 +6,28 @@ from langchain.schema import HumanMessage
 import chromadb
 
 
+def load_env():
+    load_dotenv("../.env")
+
+load_env()
+
+
+from langchain_core.prompts import PromptTemplate
+
+COLLECTION_NAME = "mail_embeddings"
+SIMILARITY_SEARCH_TYPE = "l2"
+
+RAG_PROMPT = """
+You are personal assistant that helps with email data. You are given context email data and asked to answer a question about the email.
+Context:
+{context}
+Question: {question}
+"""
+
+chromadb_instance = chromadb.PersistentClient(path="../chroma.db")
+embeddings = OpenAIEmbeddings()
+openai = OpenAI()
+chat_openai = ChatOpenAI(model_name="gpt-4")
+query_prompt = PromptTemplate(template=RAG_PROMPT, input_variables=["context", "question"])
+
+
