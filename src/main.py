ffe296ea49156a57a9af4c125d001d38eaea69bd
Shresth Sharma <shresthsharma1711@gmail.com>
2026-03-18 22:20:48 +0530

    Add events table widget

--- src/main.py
+++ src/main.py
@@ -30,3 +30,12 @@ shortened_rows = [(shorten_text(row[0]),row[1]) for row in Rows]
 
 Rows = shortened_rows
 
+class TableApp(Static):
+    def compose(self) -> ComposeResult:
+        yield DataTable()
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.add_columns(*Rows[0])
+        table.add_rows(Rows[1:])
+
